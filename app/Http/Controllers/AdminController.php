3aba212dc587079bf8c9e0b48318b698e2ff04ff
Khalid <timoncr9@gmail.com>
2018-08-04 23:25:41 +0200

    Posts' all functionality done

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -1,12 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Post;
 
 class AdminController extends Controller
 {
   public function getIndex()
   {
-    //Fetch posts and messages
-    return view('admin.index');
+    $posts = Post::orderBy('created_at' , 'desc')->take(3)->get();
+    return view('admin.index' , ['posts' => $posts]);
   }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,17 +1,106 @@
 <?php
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Validator;
+use App\Post;
 
 class PostController extends Controller
 {
+
     public function getBlogIndex()
     {
-      //Fetching post and paginate
-      return view('frontend.blog.index');
+      $posts = Post::paginate(5);
+      foreach ($posts as $post)
+      {
+          $post->body = $this->shortenText($post->body , 20);
+      }
+      return view('frontend.blog.index' , ['posts' => $posts]);
+    }
+
+    public function getPostIndex()
+    {
+      $posts = Post::paginate(5);
+      return view('admin.blog.index' , ['posts' => $posts]);
     }
 
     public function getSinglePost($post_id , $end = 'frontend')
     {
-      //Fetching post
-      return view($end . '.blog.single');
+      $post = Post::find($post_id);
+      if(!$post)
+      {
+        return redirect()->route('blog.index')->with(['fail' => 'Post not found']);
+      }
+      return view($end . '.blog.single' , ['post' => $post]);
+    }
+
+    public function getUpdatePost($post_id)
+    {
+      $post = Post::find($post_id);
+      if(!$post)
+      {
+        return redirect()->route('blog.index')->with(['fail' => 'Post not found']);
+      }
+      //Find categories
+      return view('admin.blog.edit_post' , ['post' => $post]);
+    }
+
+    public function getCreatePost()
+    {
+      return view('admin.blog.create_post');
+    }
+
+    public function postCreatePost(Request $request)
+    {
+      $request->validate([
+        'title' => 'required|max:120|unique:posts',
+        'author' => 'required|max:80',
+        'body' => 'required'
+      ]);
+
+      $post = new Post();
+      $post->title = $request['title'];
+      $post->author = $request['author'];
+      $post->body = $request['body'];
+      $post->save();
+
+      return redirect()->route('admin.index')->with(['success' => 'Post successfully created']);
+    }
+
+    public function postUpdatePost(Request $request)
+    {
+      $this->validate($request , [
+        'title' => 'required|max:120',
+        'author' => 'required|max:80',
+        'body' => 'required'
+      ]);
+      $post = Post::find($request['post_id']);
+      $post->title = $request['title'];
+      $post->author = $request['author'];
+      $post->body = $request['body'];
+      $post->update();
+      //Categories...
+      return redirect()->route('admin.index')->with(['success' => 'Post successfully updated!']);
+    }
+
+    public function getDeletePost($post_id)
+    {
+      $post = Post::find($post_id);
+      if(!$post)
+      {
+        return redirect()->route('blog.index')->with(['fail' => 'Post not found']);
+      }
+      $post->delete();
+      return redirect()->route('admin.index')->with(['success' => 'Post successfully deleted!']);
+    }
+
+    private function shortenText($text, $words_count)
+    {
+      if(str_word_count($text , 0) > $words_count)
+      {
+        $words = str_word_count($text , 2);
+        $pos = array_keys($words);
+        $text = substr($text , 0 , $pos[$words_count]) . '...';
+      }
+      return $text;
     }
 }
